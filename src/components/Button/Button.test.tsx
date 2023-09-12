d5eb3f0ebc79dd5e3adee2619037e56ebb633b14
Rafael Rojas <rafaelrojas.cov@gmail.com>
2023-09-12 11:48:05 -0300

    add testing library and create first unit test

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import { Button } from "./Button";
+
+describe("button", () => {
+  it("should render button", () => {
+    render(<Button />);
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should render button with text", () => {
+    render(<Button>Button</Button>);
+    const button = screen.getByText("Button");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should trigger onclick function", () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick} />);
+    const button = screen.getByRole("button");
+
+    button.click();
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("should render basic variant as default", () => {
+    render(<Button />);
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveClass("button");
+    expect(button).toHaveClass("button__basic");
+  });
+
+  it("should render primary variant when prop is passed", () => {
+    render(<Button variant="primary" />);
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveClass("button");
+    expect(button).toHaveClass("button__primary");
+  });
+
+  it("should render basic variant with active prop when prop is passed", () => {
+    render(<Button active />);
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveClass("button");
+    expect(button).toHaveClass("button__basic--active");
+  });
+
+  it("should render primary variant with active prop when prop is passed", () => {
+    render(<Button variant="primary" active />);
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveClass("button");
+    expect(button).toHaveClass("button__primary--active");
+  });
+});
