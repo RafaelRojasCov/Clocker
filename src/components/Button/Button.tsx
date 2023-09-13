9025b9718487860a68e436805b78c3cd1cd6a6d0
Rafael Rojas <rafaelrojas.cov@gmail.com>
2023-09-13 19:43:34 -0300

    add awesome icons and reset button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,28 +1,40 @@
 import classNames from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Button.module.scss";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 interface ButtonProps {
   children?: React.ReactNode;
   onClick?: () => void;
   variant?: string;
   active?: boolean;
+  icon?: IconProp;
+  className?: string;
 }
 
 export const Button: React.FC<ButtonProps> = ({
-  children = "Button",
+  children,
   onClick,
   variant = "basic",
   active,
+  icon,
+  className,
 }) => {
   return (
     <button
       onClick={onClick}
-      className={classNames(styles.button, {
-        [styles[`button__${variant}`]]: variant,
-        [styles[`button__${variant}--active`]]: variant && active,
-      })}
+      className={classNames(
+        styles.button,
+        {
+          [styles[`button__${variant}`]]: variant,
+          [styles[`button__${variant}--active`]]: variant && active,
+          [styles[`button__icon`]]: icon,
+        },
+        className
+      )}
     >
       {children}
+      {icon && <FontAwesomeIcon icon={icon} size="2xl" />}
     </button>
   );
 };
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,16 @@
 import React, { useReducer, useState, useEffect } from "react";
+import classNames from "classnames";
 import { Container } from "../Container";
 import { Button } from "../Button";
 import { Timer } from "../Timer";
 import mouseClick from "../../assets/sounds/mouseClick.mp3";
 import { pomodoro, shortBreak, longBreak, Clock } from "./Clock";
+import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./Home.module.scss";
 
 const Home: React.FC = () => {
-  // remove this hack when change approach for Clock.tsx
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [ignored, forceUpdate] = useReducer((x) => x + 1, 0);
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
   const [currentTimer, setCurrentTimer] = useState<Clock>(pomodoro);
   const clickSound = new Audio(mouseClick);
   const {
@@ -23,14 +24,15 @@ const Home: React.FC = () => {
     isRunning,
   } = currentTimer;
 
-  const handleReset = (timer: Clock) => () => {
-    if (currentTimer === timer) return;
-    const confirmation =
-      isRunning && window.confirm("¿Quieres reiniciar el timer?");
-    if (isRunning && !confirmation) return;
-    timer.reset(timer.defaultMinutes);
-    setCurrentTimer(timer);
-  };
+  const handleReset =
+    (timer: Clock, options?: { forceReset?: boolean }) => () => {
+      if (currentTimer === timer && !options?.forceReset) return;
+      const confirmation =
+        isRunning && window.confirm("¿Quieres reiniciar el timer?");
+      if (isRunning && !confirmation) return;
+      timer.reset(timer.defaultMinutes);
+      setCurrentTimer(timer);
+    };
 
   const handleStartStop = () => {
     clickSound.play();
@@ -73,9 +75,30 @@ const Home: React.FC = () => {
 
         <Timer minutes={minutes} seconds={seconds} />
 
-        <Button variant="primary" active={isRunning} onClick={handleStartStop}>
-          {isRunning ? "Pause" : "Start"}
-        </Button>
+        <div
+          className={classNames(styles["home__actions"], {
+            [styles["home__actions--running"]]: isRunning,
+          })}
+        >
+          <Button
+            variant="primary"
+            active={isRunning}
+            onClick={handleStartStop}
+          >
+            {isRunning ? "Pause" : "Start"}
+          </Button>
+          {isRunning && (
+            <>
+              <Button
+                className={classNames({
+                  [styles[`home__button-reset--running`]]: isRunning,
+                })}
+                icon={faRotateLeft}
+                onClick={handleReset(currentTimer, { forceReset: true })}
+              />
+            </>
+          )}
+        </div>
       </Container>
     </div>
   );
